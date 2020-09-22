528dda5a38b8dd794f9a2983b2cd976f43090846
lucas-besen <lucas.andrebesen@gmail.com>
2020-09-21 23:47:34 -0300

    distance

---
+++ 1044.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+main(){
+	int a,b;	
+    cin>>a>>b;
+	if (a%b==0||b%a==0){
+		cout<<"Sao Multiplos\n";
+	}
+	else{
+		cout<<"Nao sao Multiplos\n ";
+	}
+    return 0;
+}
+
