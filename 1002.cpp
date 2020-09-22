298e2f0abebfb96fe0455a669d7e8fe6b3607cb6
lucas-besen <lucas.andrebesen@gmail.com>
2020-09-21 23:13:20 -0300

    area of a circle

---
+++ 1002.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main(){	 
+    double raio,pi=3.14159,area_do_circulo;
+    cin>>raio;
+    area_do_circulo=pow(raio,2)*pi;
+    printf("A=%.4f\n",area_do_circulo);
+    return 0; 
+    }
