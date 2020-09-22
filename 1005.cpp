fcc9fd280d10c375a3e38911d728ad656199164d
lucas-besen <lucas.andrebesen@gmail.com>
2020-09-21 23:15:09 -0300

    average 1

---
+++ 1005.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+main(){	
+    float pesoA=3.5, pesoB=7.5, media=0,a,b;
+    cin>>a>>b;
+    a=a*pesoA;
+    b=b*pesoB;
+    media=a+b;
+    media=media/11;
+    printf("MEDIA = %.5f\n",media);
+    return 0;
+}   
+ 
+
