b99c892c688907dd4f0354f3e8d7278867df8471
lucas-besen <lucas.andrebesen@gmail.com>
2020-09-26 20:10:11 -0300

    game time with minutes

--- 1047.cpp
+++ 1047.cpp
@@ -6,20 +6,29 @@ using namespace std;
 
 main(){
 
-    int HoraInicial, MinutoInicial,HoraFinal,MinutoFinal;
-    float MinutosTotal;
+    int HoraInicial, MinutoInicial,HoraFinal,MinutoFinal,MinutosTotal,HoraTotal;
     cin>>HoraInicial>>MinutoInicial>>HoraFinal>>MinutoFinal;
     MinutoFinal+=HoraFinal*60;
     MinutoInicial+=HoraInicial*60;
+    
     if (MinutoFinal==MinutoInicial){
-       cout<<"O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)";
+      
+       HoraTotal=24;
+       MinutosTotal=0;
     }
     else if (MinutoInicial<MinutoFinal){
-        MinutosTotal=-1*(MinutoInicial-MinutoFinal);       
+       
+        MinutosTotal=(MinutoFinal-MinutoInicial);
+        HoraTotal=MinutosTotal/60;
+        MinutosTotal=MinutosTotal-(HoraTotal*60);    
+    }
+    else if (MinutoInicial>MinutoFinal){
+        MinutosTotal=(1440-MinutoInicial)+MinutoFinal;
+        HoraTotal=MinutosTotal/60;
+        MinutosTotal= MinutosTotal-(HoraTotal*60);
     }
     
-    
-    
+    cout<<"O JOGO DUROU "<< HoraTotal <<" HORA(S) E "<< MinutosTotal <<" MINUTO(S)\n";
 
     return 0;
 }
